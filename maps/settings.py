abf1893055bbdcf70c89a7a7ca2094e40e06c1b5
Ubuntu <ubuntu@ip-172-31-89-227.ec2.internal>
2020-05-01 12:14:56 +0000

    changeMaps marker display bug fix

--- maps/settings.py
+++ maps/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
+    #addons
+    'bootstrap4',
+    
     #local
     'streetmarkers.apps.StreetmarkersConfig',
 ]
